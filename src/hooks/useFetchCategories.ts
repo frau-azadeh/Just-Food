77d8ca5e606d1246bf2504f6eb4d60ad4ff64939
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2024-12-30 02:35:50 -0800

    add useFetchCategories

---
+++ src/hooks/useFetchCategories.ts
@@ -0,0 +1,34 @@
+
+import { useState, useEffect } from "react";
+import api from "@/utils/api";
+
+type Category = {
+  idCategory: string;
+  strCategory: string;
+  strCategoryThumb: string;
+  strCategoryDescription: string;
+};
+
+export const useFetchCategories = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await api.get("/categories.php"); // Use base URL + endpoint
+        setCategories(response.data.categories);
+      } catch (err) {
+        setError("Failed to fetch categories");
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  return { categories, loading, error };
+};
