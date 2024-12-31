9a9428a1a892281679851303818ad4a741c5688c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2024-12-31 06:30:25 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CategoriesCarousel from '@/components/CategoriesCarousel';
 import ImageSlider from '@/components/ImageSlider';
+import RandomMeals from '@/components/RandomMeals';
 
 const Page: React.FC = () => {
   return (
     <div>
       <ImageSlider/>
       <CategoriesCarousel />
+      <RandomMeals/>
     </div>
   );
 };
